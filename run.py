fb8d55a846f4166eca59ea147ac783b013939395
jaycariaga <jay_jay_xray@yahoo.com>
2019-07-03 11:31:20 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+from eve import Eve
+
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
+
+app = Eve()
+#if _name_ == '_main_':
+app.run(host=host, port=port)
