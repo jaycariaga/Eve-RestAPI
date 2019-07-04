3c64c15795d6cb60e5ecc230dcf75003a070c251
jaycariaga <jay_jay_xray@yahoo.com>
2019-07-03 21:37:02 -0400

    Update run.py

--- run.py
+++ run.py
@@ -11,6 +11,11 @@ else:
     port = 5000
     host = '127.0.0.1'
 
+#settings = {'DOMAIN':{'people':{}}}
+
 app = Eve()
-#if _name_ == '_main_':
-app.run(host=host, port=port)
+if __name__ == '__main__':
+	#app.debug = True
+	app.run()
+
+#host=host, port=port  : is to post inside app.run()
