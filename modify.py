2dfdbf2ff22b89bac28b3268bfd4910da5c2e460
jaycariaga <jay_jay_xray@yahoo.com>
2019-07-17 14:27:13 -0400

    Update modify.py

--- modify.py
+++ modify.py
@@ -98,9 +98,13 @@ def endpoint(resource):
         ENTRY_POINT if not sys.argv[1:] else sys.argv[1], resource)
 
 #works
+def checkdeleteitem(resource, ident, etag):
+    r = delitem(resource, ident, etag)
+    print r.status_code
+
 def delitem(resource, ident, etag): 
     url_ = "%s%s/" % (endpoint(resource), ident)
-    print url_ 
+    print url_ #checking for correct url
     return requests.delete(url_, headers = {"IF-MATCH": etag}) #can use global configs in headers
 
 #works and returns whole endpoint
@@ -111,15 +115,46 @@ def get_people(resource):
 def get_person(resource, ident):
     print '%s' % ((requests.get(endpoint(resource)+ident)).json())
 
-	
+#works
+def patching(resource, ident, etag, change):
+    url_ = "%s%s/" % (endpoint(resource), ident)
+    return requests.patch(url_, json=change , headers = {"IF-MATCH": etag})
+#json={"firstname" : "blah"} is an example of second param
 
+def checkput(resource, ident, etag, changes):
+    out = put(resource, ident, etag, changes)
+    print "'put' status: ", out.status_code
 
+def put(resource, ident, etag, changes):
+    url_ = "%s%s/" % (endpoint(resource), ident)
+    
+    return requests.put(url_, json = changes, headers = {"IF-MATCH": etag})
+
+#REMEMBER: patch and put is an updater, so the etag always changes after!
+""" The running code below contain samples of my function calls that involve 
+multiple important REST API's: POST, GET, DELETE, PATCH, and PUT.
+The get_people function prints all items of a resource, while get_person gets a specific person based on 
+specifying identification. Delete, Patch, and Put work differently in that they require an IF-MATCH header."""
 if __name__ == '__main__':
-    delitem('people','5d2e37d85d1da9dedaae26aa', '7a5a701ee5a0acba6bf27ca1a3094ee6effdca6d')
+    #checkdeleteitem('people','5d2e37d85d1da9dedaae26ab', 'c7844155ef09f7ff5a957c45eb75955f650fc8bb') 
+    #deleting an item REQUIRES id and not the unique variable of 'lastname'!
+
     #get_people('people')
+
     #get_person('people', 'Doe') #can use either lastname 'Doe' or the id of that item
 
-    #from previous known code
-    #delete()
-    #ids = post_people()
-    #post_works(ids)
+    #patching('people', '5d2e37d85d1da9dedaae26ab', '5ef1599d4b66578d6f2c76ae0f0dbfc3027bfe7f', {"firstname": 'chance'})
+
+    #checkput('people', '5d2e37d85d1da9dedaae26ad', '2b32dd3a178770f28e78876d4e922965490ec1fc', {
+    #        'firstname': 'Mark',
+    #        'lastname': 'Red', #lastname is a unique-schema so it CANT be changed...must specify that it stays the same or put wont work
+    #        'role': ['copy', 'author'],
+    #        'location': {'address': '4925 Lacross Road', 'city': 'New York'},
+    #        'born': 'Sat, 23 Feb 1985 12:00:00 GMT'
+    #    })
+    #get_people('people')
+    
+    #code below came from a tutorial that deletes both resources and replaces it with defaults using DELETE and POSTS...
+    delete()
+    ids = post_people()
+    post_works(ids)
